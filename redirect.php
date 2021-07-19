a805ffecb17c61cfcbb29d7ec8e7c2f96237bbce
SD-IITKGP <82934534+SD-IITKGP@users.noreply.github.com>
2021-07-19 13:01:52 +0530

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -2,8 +2,8 @@
 $insert=false;
 if(isset($_POST['name'])){
 $server = "localhost";
-$username = "id16501941_rca";
-$password ="Sourabh1503.";
+$username = "************";
+$password ="***********";
 $connection=mysqli_connect($server,$username,$password);
 if(!$connection)
 {
@@ -12,7 +12,7 @@ if(!$connection)
 $name=$_POST['name'];
 $email=$_POST['email'];
 $message=$_POST['message'];
-$sql ="INSERT INTO `id16501941_form`.`form` (`name`, `email`, `message`, `ct`) VALUES('$name', '$email', '$message', current_timestamp());";
+$sql ="INSERT INTO `*************`.`form` (`name`, `email`, `message`, `ct`) VALUES('$name', '$email', '$message', current_timestamp());";
 if($connection->query($sql)==True)
 {
 $insert=true;
